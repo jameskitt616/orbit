fefb3753bcb7aa8e57713d420cdde5c1f23c4af3
jameskitt616 <52933658+jameskitt616@users.noreply.github.com>
2023-04-29 20:59:03 +0200

    Add ffmpeg import test

--- Dockerfile
+++ Dockerfile
@@ -105,7 +105,7 @@ RUN set -eux; \
 	mkdir -p var/cache var/log; \
     if [ -f composer.json ]; then \
 		composer dump-autoload --classmap-authoritative --no-dev; \
-		composer dump-env prod; \
+    	composer dump-env prod; \
 		composer run-script --no-dev post-install-cmd; \
 		chmod +x bin/console; sync; \
     fi
--- src/Stream/Presentation/Controller/StreamController.php
+++ src/Stream/Presentation/Controller/StreamController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Stream\Presentation\Controller;
 
+use FFMpeg\FFMpeg;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,7 +14,19 @@ final class StreamController extends AbstractController
     #[Route(path: '/stream', name: 'stream', methods: ['GET'])]
     public function show(): Response
     {
-        return $this->render('security/login.html.twig', [
-        ]);
+        $config = [
+            'ffmpeg.binaries'  => '/usr/bin/ffmpeg',
+            'ffprobe.binaries' => '/usr/bin/ffprobe',
+            'timeout'          => 3600,
+            'ffmpeg.threads'   => 12,
+        ];
+
+        $ffmpeg = FFMpeg::create($config);
+//        $video = $ffmpeg->open('');
+//        dump($video);
+
+        return new Response('asd');
+//        return $this->render('security/login.html.twig', [
+//        ]);
     }
 }
