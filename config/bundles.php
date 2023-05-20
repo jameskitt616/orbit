6b444f29c07331f0bdcac7b404ee43f5c46c8b7d
jameskitt616 <52933658+jameskitt616@users.noreply.github.com>
2023-05-20 09:47:08 +0200

    remove deprecated sensio/framework-extra-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -7,7 +7,6 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
