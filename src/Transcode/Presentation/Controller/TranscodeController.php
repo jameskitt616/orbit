9fb9e01e2ae8efa8f0f9aa089aa7754e615e1ab9
jameskitt616 <52933658+jameskitt616@users.noreply.github.com>
2023-05-21 17:07:38 +0200

    Add debug function

--- src/Transcode/Presentation/Controller/TranscodeController.php
+++ src/Transcode/Presentation/Controller/TranscodeController.php
@@ -123,4 +123,12 @@ final class TranscodeController extends AbstractController
 
         return $this->redirectToRoute('transcode_list');
     }
+
+    #[Route(path: '/show/{transcode}/go', name: 'transcode_go', methods: ['GET'])]
+    public function go(Transcode $transcode): Response
+    {
+        $this->transcodeService->transcode($transcode);
+
+        return $this->redirectToRoute('transcode_list');
+    }
 }
